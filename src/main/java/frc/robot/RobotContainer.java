522a66fe81101b9a6fa5c02487809b442eeacb6c
ripsaw7797 <ripsawrobotics7797@gmail.com>
2020-01-30 14:56:36 -0800

    new comment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,8 +20,6 @@ import edu.wpi.first.wpilibj2.command.Command;
  * actually be handled in the {@link Robot} periodic methods (other than the
  * scheduler calls). Instead, the structure of the robot (including subsystems,
  * commands, and button mappings) should be declared here.
- * 
- * 
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
